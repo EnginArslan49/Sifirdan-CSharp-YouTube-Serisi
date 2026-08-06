05586952db949a238127b82594ae3dc81871f78d
Engin Arslan <arslanengin3175@gmail.com>
2026-08-06 19:39:01 +0300

    Video 4 kapsamında Değişkenler ve Değişken Tanımlama Konusunun proje klasörü eklendi!

---
+++ 002-DegiskenNedirveDegiskenTanımlama/002-DegiskenNedirveDegiskenTanımlama/Program.cs
@@ -0,0 +1,26 @@
+﻿// DEĞİŞKEN TANIMLAMA
+// veriTipi degiskenAdi = deger;    👈 değişkeni tanımaldık ve değer atadık.
+
+// Önce tanımlanıp daha sonra değer verilebilir.
+// veriTipi degiskenAdi;
+
+int Sinif;
+
+// Değişkene değer atanıyor.
+Sinif = 9;
+
+// Değişkenin değeri değiştirilebilir.
+Sinif = 10;
+
+
+// string: Metin saklar. (" ")
+// char: Tek karakter saklar. (' ')
+
+string Ad = "Ali";
+char Cinsiyet = 'E';
+
+
+// Değişkenleri ekrana yazdırıyoruz.
+Console.WriteLine("Öğrencinin Sınıf Düzeyi: " + Sinif);
+Console.WriteLine("Ad: " + Ad);
+Console.WriteLine("Cinsiyet: " + Cinsiyet);

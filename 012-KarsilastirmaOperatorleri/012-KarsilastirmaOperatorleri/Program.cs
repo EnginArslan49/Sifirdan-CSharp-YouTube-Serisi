55a4d976b408ee79b3fad43d000abec857fe4f13
Engin Arslan <arslanengin3175@gmail.com>
2026-08-16 14:35:48 +0300

    Ders 15 kapsamında işlenen Karşılaştırma Operatörleri konusuna ilişkin proje dosyaları 012-KarsilastirmaOperatorleri klasörü altına eklendi!

---
+++ 012-KarsilastirmaOperatorleri/012-KarsilastirmaOperatorleri/Program.cs
@@ -0,0 +1,197 @@
+﻿/*
+    ============================================================
+        C# SIFIRDAN EĞİTİM
+        DERS 15 - KARŞILAŞTIRMA OPERATÖRLERİ
+    ============================================================
+
+    KARŞILAŞTIRMA OPERATÖRLERİ:
+
+    Karşılaştırma operatörleri, iki değeri karşılaştırmak
+    ve karşılaştırmanın sonucunda true veya false elde etmek
+    için kullanılır.
+
+
+    🌟🌟 ÖNEMLİ!: Karşılaştırma işlemlerinin tümünde sonuç;
+        true veya false
+    değer verecektir
+*/
+
+
+
+
+
+
+
+
+
+/*
+    ------------------------------------------------------------
+        EŞİTTİR OPERATÖRÜ (==)
+    ------------------------------------------------------------
+
+    İki değerin birbirine eşit olup olmadığını kontrol eder.
+*/
+
+/*
+Console.WriteLine( 5 == 9 );    // false
+
+Console.WriteLine( "ARSLAN" == "ARSLAN" ); // True
+*/
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*
+    ------------------------------------------------------------
+        EŞİT DEĞİLDİR OPERATÖRÜ (!=)
+    ------------------------------------------------------------
+
+    İki değerin birbirine eşit olmadığını kontrol eder.
+*/
+
+/*
+Console.WriteLine( 5 != 9 );    // True
+
+Console.WriteLine( 5 != 5 );    // False
+
+Console.WriteLine( "Hüma" != "Hüma" ); // False
+*/
+
+
+
+
+
+
+
+
+/*
+    ------------------------------------------------------------
+        BÜYÜKTÜR OPERATÖRÜ (>)
+    ------------------------------------------------------------
+
+    Sol taraftaki değerin sağ taraftaki değerden büyük
+    olup olmadığını kontrol eder.
+*/
+
+/*
+Console.WriteLine( 5 > 9 );     // False
+
+Console.WriteLine( 5 > 4 );     // True
+
+Console.WriteLine( 5 > 5 );     // False
+*/
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*
+    ------------------------------------------------------------
+        KÜÇÜKTÜR OPERATÖRÜ (<)
+    ------------------------------------------------------------
+
+    Sol taraftaki değerin sağ taraftaki değerden küçük
+    olup olmadığını kontrol eder.
+*/
+
+/*
+Console.WriteLine( 5 < 9 ); // True
+
+Console.WriteLine( 5 < 4 ); // False
+
+Console.WriteLine( 5 < 5 ); // False
+*/
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*
+    ------------------------------------------------------------
+        BÜYÜK EŞİTTİR OPERATÖRÜ (>=)
+    ------------------------------------------------------------
+
+    Sol taraftaki değerin sağ taraftaki değerden büyük
+    veya eşit olup olmadığını kontrol eder.
+*/
+
+/*
+Console.WriteLine( 5 >= 5 ); // True
+
+Console.WriteLine( 5 >= 4 ); // True
+
+Console.WriteLine( 5 >= 6 ); // False
+*/
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*
+    ------------------------------------------------------------
+        KÜÇÜK EŞİTTİR OPERATÖRÜ (<=)
+    ------------------------------------------------------------
+
+    Sol taraftaki değerin sağ taraftaki değerden küçük
+    veya eşit olup olmadığını kontrol eder.
+*/
+
+
+Console.WriteLine( 4 <= 5 ); // True
+
+Console.WriteLine( 4 <= 4 ); // True
+
+Console.WriteLine( 4 <= 3 ); // False
